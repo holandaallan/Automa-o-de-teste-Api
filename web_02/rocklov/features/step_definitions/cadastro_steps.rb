3e2c0da5bae09fc260c365ebd58393f95bfdb865
Allan Gomes de Holanda <holanda.allan@gmail.com>
2021-02-09 10:30:09 -0300

    Primeiro commit

---
+++ web_02/rocklov/features/step_definitions/cadastro_steps.rb
@@ -0,0 +1,17 @@
+
+Dado('que acesso a página de cadastro') do
+    visit "/signup"
+end
+
+Quando('submeto o seguinte formulário de cadastro:') do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    user = table.hashes.first
+
+    MongoDB.new.remove_user(user[:email])
+
+    find("#fullName").set user[:nome]
+    find("#email").set user[:email]
+    find("#password").set user[:senha]
+
+    click_button "Cadastrar"
+end
